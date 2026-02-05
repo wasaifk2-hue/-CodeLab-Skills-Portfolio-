2c23d4b512feaf3405e92d89351232baadfc996e
wasaifk2-hue <wasaifk2@gmail.com>
2026-02-05 14:04:23 +0300

    This program shows a basic information
     Biography

---
+++ biography03.py
@@ -0,0 +1,14 @@
+# This program shows a basic information
+# Author: wasaif Diab
+# Date: 29/12/2025
+name = "wasaif"
+hometown ="riyadh"
+age = 26
+
+user_info = {
+    "Name": name,
+    "Hometown": hometown,
+    "Age": age
+}
+
+print(user_info["Name"], "\n" + user_info["Hometown"], "\n" + str(user_info["Age"]))
