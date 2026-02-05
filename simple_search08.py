524f3ba2da9db6bd88d2168f3735d04895763e5b
wasaifk2-hue <wasaifk2@gmail.com>
2026-02-05 14:17:17 +0300

    this program searches for a number in a list
     Simple Search

---
+++ simple_search08.py
@@ -0,0 +1,12 @@
+#this program searches for a number in a list
+# Author: wasaif Diab
+# Date: 29/12/2025
+
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+
+search_term = input("Enter the name to search: ")
+
+if search_term in names:
+    print(search_term, "found in the list!")
+else:
+    print(search_term, "not found in the list.")
