11ed577f623b0367f799651d6d2783a276b0bf4e
wasaifk2-hue <wasaifk2@gmail.com>
2026-02-05 14:15:28 +0300

    this program counts from 1 to 10
    Some Counting

---
+++ some_counting07.py
@@ -0,0 +1,27 @@
+#this program counts from 1 to 10
+# Author: wasaif Diab
+# Date: 29/12/2025
+
+for i in range(0, 51):
+    print(i)
+
+print("----")
+
+for i in range(50, -1, -1):
+    print(i)
+
+print("----")
+
+for i in range(30, 51):
+    print(i)
+
+print("----")
+
+for i in range(50, 9, -2):
+    print(i)
+
+print("----")
+
+for i in range(100, 201, 5):
+    print(i)
+print("----")
