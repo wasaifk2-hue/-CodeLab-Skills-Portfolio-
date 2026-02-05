6110b032066cf5bdd3ba0532bfb8ae490353e256
wasaifk2-hue <wasaifk2@gmail.com>
2026-02-05 14:08:07 +0300

    This program asks a simple question
     Primitive Quiz

---
+++ primitive_quiz04.py
@@ -0,0 +1,70 @@
+# This program asks a simple question
+# Author: wasaif Diab
+# Date: 29/12/2025
+
+answer = input("What is the capital of France? ")
+
+if answer.lower() == "paris":
+    print("Correct answer")
+else:
+    print("Wrong answer")
+answer = input("What is the capital of Germany? ")
+if answer.lower() == "berlin":
+    print("Correct answer")
+else:
+    print("Wrong answer")
+
+
+answer = input("What is the capital of Italy? ")
+if answer.lower() == "rome":
+    print("Correct answer")
+else:
+    print("Wrong answer")
+
+
+answer = input("What is the capital of Spain? ")
+if answer.lower() == "madrid":
+    print("Correct answer")
+else:
+    print("Wrong answer")
+
+
+answer = input("What is the capital of Japan? ")
+if answer.lower() == "tokyo":
+    print("Correct answer")
+else:
+    print("Wrong answer")
+
+
+answer = input("What is the capital of China? ")
+if answer.lower() == "beijing":
+    print("Correct answer")
+else:
+    print("Wrong answer")
+
+
+answer = input("What is the capital of Canada? ")
+if answer.lower() == "ottawa":
+    print("Correct answer")
+else:
+    print("Wrong answer")
+
+
+answer = input("What is the capital of Brazil? ")
+if answer.lower() == "brasilia":
+    print("Correct answer")
+else:
+    print("Wrong answer")
+
+
+answer = input("What is the capital of Egypt? ")
+if answer.lower() == "cairo":
+    print("Correct answer")
+else:
+    print("Wrong answer")
+
+answer = input("What is the capital of Australia? ")
+if answer.lower() == "canberra":
+    print("Correct answer")
+else:
+    print("Wrong answer")
