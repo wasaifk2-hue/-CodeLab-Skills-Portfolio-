387e3702f2c0f3d10f387d7589bc574155682155
wasaifk2-hue <wasaifk2@gmail.com>
2026-02-05 14:13:44 +0300

    this program keeps asking for password
    Brute Force Attack

---
+++ brute_force_attack06.py
@@ -0,0 +1,13 @@
+#this program keeps asking for password
+# Author: wasaif Diab
+# Date: 29/12/2025
+
+password = "12345"
+while True:
+    
+    pwd=input("Enter the password: ")
+    if pwd == password:
+        print("welcome")
+        break
+    else:
+        print("wrong password")
